0637d816cdab4cb0702a28b98a6f4f1dfb859153
Caprena Richie <robcaprena@gmail.com>
2021-09-30 02:59:59 -0400

    first commit

---
+++ SelectionStatementsExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SelectionStatementsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int favoriteNumber = 10;
+
+            Console.Write("Try to guess my favorite number");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess <= favoriteNumber)  
+            {
+                Console.WriteLine("too low!!");
+            }
+            else if (userGuess >= favoriteNumber) 
+            {
+                Console.WriteLine("too high!");
+
+            }
+            else 
+            {
+                Console.WriteLine("Nevermind");
+            }            
+            
+             
+                      
+
+            
+        }
+    }
+}
